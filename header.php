f0fa4fd193f37ad9377c9989007bb8070c2f5527
Jeremy Ward <jeremy.ward@webdevstudios.com>
2018-06-11 22:55:12 -0500

    Add some style adjustments for a quick update tonight.

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* The theme header file.
+ * The theme header file.
  *
  * @package JMW\GermBlog
  */
@@ -9,13 +9,18 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <header class="site-header">
-    <nav class="site-nav">
-        <?php wp_nav_menu(); ?>
-    </nav>
+	<div class="site-header__info">
+		<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+		<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+	</div>
+
+	<nav class="site-nav">
+		<?php wp_nav_menu(); ?>
+	</nav>
 </header>
 
 <div class="wrapper">
