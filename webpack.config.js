d42f95bb2dd21d3ba0a2cf055cb43b208da7400d
Jeremy Ward <jeremy.ward@webdevstudios.com>
2018-07-28 12:52:51 -0400

    Format webpack config file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,15 @@
 var Encore = require('@symfony/webpack-encore');
 
-Encore
-.setOutputPath('./assets/dist')
-.setPublicPath('/wp-content/themes/germ-blog-theme/assets/dist')
-.addEntry('app', './assets/src/js/app.js')
-.autoProvidejQuery()
-// .enableVersioning(Encore.isProduction())
-    .enableSourceMaps(!Encore.isProduction())
-    .cleanupOutputBeforeBuild()
-.enableBuildNotifications()
-.enableSassLoader()
-.configureUrlLoader()
+Encore.setOutputPath('./assets/dist').
+    setPublicPath('/wp-content/themes/germ-blog-theme/assets/dist').
+    addEntry('app', './assets/src/js/app.js').
+    autoProvidejQuery()
+    // .enableVersioning(Encore.isProduction())
+    .enableSourceMaps(!Encore.isProduction()).
+    cleanupOutputBeforeBuild().
+    enableBuildNotifications().
+    enableSassLoader().
+    configureUrlLoader()
 ;
 
 module.exports = Encore.getWebpackConfig();
